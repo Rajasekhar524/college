bb7b8ffc12e5fd0b91a863a8b66fc5c4ff3d5f7f
Raj <rajshekhar.k321@gmail.com>
2021-02-28 23:24:04 +0530

    make it better

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 //middleware
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cors())
+app.use(cors({ origin: true }));
 
 app.set('view engine','ejs')
 
