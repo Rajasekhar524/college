8f3ae7350dc1cfbc70af37b860536dc4897422dd
Raj <rajshekhar.k321@gmail.com>
2021-02-28 21:21:26 +0530

    heroku

--- server/app.js
+++ server/app.js
@@ -16,11 +16,17 @@ app.set('view engine','ejs')
 
 const dbURI = 'mongodb+srv://Raj:211221@cluster1.mg6wb.mongodb.net/node-auth?retryWrites=true&w=majority'
 
-mongoose.connect(dbURI,{useNewUrlParser:true,
+mongoose.connect(process.env.MONGODB_URI||dbURI,{useNewUrlParser:true,
                         useUnifiedTopology:true,
                         useCreateIndex:true })
                         .then((result)=> app.listen(3000))
                         .catch((err)=>console.log(err));
+
+if(process.envNODE_ENV ==='production') {
+    app.use(express.static('client/build'))
+}
+
+
 
 app.get('/', (req,res) => res.render('home'))
 app.get('/smooth',(req,res)=> res.render('smooth'));
